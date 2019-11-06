b32e8c29822a83a997c7fb1abec52adcb7591b8b
David-Flores-Ruiz <39530033+David-Flores-Ruiz@users.noreply.github.com>
2019-11-06 15:39:04 -0800

    Archivos inicialesd de I2C .c y .h

---
+++ I2C.c
@@ -0,0 +1,11 @@
+/*
+ * I2C.c
+ *
+ *  Created on: Nov 6, 2019
+ *      Author: David Ruiz
+ */
+
+#include <stdint.h>
+#include "I2C.h"
+
+
---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2016-2019 NXP
+ * All rights reserved.
+ *
+ * @file    T11_I2C_vClass.c
+ * @brief   Application entry point.
+ */
+#include <stdio.h>
+#include "MK64F12.h"
+#include "I2C.h"
+
+/*
+ * @brief   Application entry point.
+ */
+int main(void) {
+
+  	/* Init board hardware. */
+
+
+    return 0 ;
+}
