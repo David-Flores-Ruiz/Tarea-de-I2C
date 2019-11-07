3de0ce91bb948c1fe09c4777b98a14f8ea940f53
David Flores Ruiz <ie717807@iteso.mx>
2019-11-07 00:31:54 -0600

    Primeras funciones en .C del I2C

--- I2C.c
+++ I2C.c
@@ -5,12 +5,26 @@
  *      Author: David Ruiz
  */
 
+
 #include "I2C.h"
 
 
 void I2C_init(i2c_channel_t channel, uint32_t system_clock, uint16_t baud_rate) {
 	if (I2C_0 == channel) {
 		SIM->SCGC4 |= I2C0_CLOCK_GATING;	/* Bit_6 = 0x40h*/
+		/**	Configurar el Clock Gating de los perifericos GPIO a utilizar */
+		GPIO_clock_gating(GPIO_B);
+		/** Utiliza la alternativa 2 para I2C_SCL(reloj) y I2C_SDA(datos) */
+		gpio_pin_control_register_t I2C_config = GPIO_MUX2;
+		/** Asignamos los pines de salidas del protocolo de comunicacion I2C */
+		GPIO_pin_control_register(GPIO_B, bit_2, &I2C_config);
+		GPIO_pin_control_register(GPIO_B, bit_3, &I2C_config);
+		GPIO_data_direction_pin(GPIO_B, GPIO_OUTPUT, bit_2); //SCL Reloj
+		GPIO_data_direction_pin(GPIO_B, GPIO_OUTPUT, bit_3); //SDA Dato
+
+
+
+
 	}
 	if (I2C_1 == channel) {
 		SIM->SCGC4 |= SIM_SCGC4_I2C1_MASK;	/* Bit_7 = 0x80h*/
@@ -18,10 +32,4 @@ void I2C_init(i2c_channel_t channel, uint32_t system_clock, uint16_t baud_rate) 
 	if (I2C_2 == channel) {
 		SIM->SCGC1 |= SIM_SCGC1_I2C2_MASK;	/* Bit_6 = 0x40h*/
 	}
-
-	/**	Configurar el Clock Gating de los perifericos GPIO a utilizar */
-	GPIO_clock_gating(GPIO_B);
-
-
-
 }
--- main.c
+++ main.c
@@ -1,28 +1,32 @@
 /*
- * Copyright 2016-2019 NXP
- * All rights reserved.
- *
- * @file    T11_I2C_vClass.c
- * @brief   Application entry point.
+ * @file:			I2C_vClass.c
+ * @company:		ITESO
+ * @Engineer Team:	D.F.R. / R.G.P.
  */
+
 #include <stdio.h>
 #include "MK64F12.h"
 #include "I2C.h"
 
+
 #define SYSTEM_CLOCK (21000000U)
 #define BAUD_RATE (9600U)
 
-/*
- * @brief   Application entry point.
- */
+
 int main(void) {
+
 	I2C_init(I2C_0, SYSTEM_CLOCK, BAUD_RATE);
 
 
-  	/* Init board hardware. */
 
+    while(1) {
+
+    	////
+
+    }
 
 
 
     return 0 ;
 }
+
